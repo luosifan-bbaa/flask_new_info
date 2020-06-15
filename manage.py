9907ee9e23910a66d4102b02864cf5b9d3440fcd
luosifan-bbaa <64411917+luosifan-bbaa@users.noreply.github.com>
2020-06-15 22:22:30 +0800

    项目拆分

--- manage.py
+++ manage.py
@@ -1,11 +1,23 @@
-from flask import Flask
+# -*- coding: utf-8 -*-
+# @Time    : 2020/6/15 17:43
+# @Author  : Eric Lee
+# @Email   : li.yan_li@neusoft.com
+# @File    : manage.py 只负责基本的启动工作，
+# app 的创建在 info 下的__init__ 中
+# @Software: PyCharm
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from info import app, db
+# flask_script
+manager = Manager(app)
+# 数据库迁移
+Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
-app = Flask(__name__)
 
 @app.route('/')
-
 def index():
-    return"index"
-
+    session['name'] = 'eric'
+    return "index"
 if __name__ == '__main__':
-    app.run()
+    manager.run()
